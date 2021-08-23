af8e97cd81a4ec875d5429503f47a73bb1a7e9b5
Yauheny Bychkou <375445433672@yandex.by>
2021-08-23 08:57:30 +0300

    ready

--- script/script.js
+++ script/script.js
@@ -147,14 +147,55 @@ window.addEventListener("DOMContentLoaded", function () {
   };
 
   toggleMenu();
+
   const togglePopUp = () => {
     const popup = document.querySelector(".popup"),
       popupBtn = document.querySelectorAll(".popup-btn"),
+      hightWindow = window.innerWidth,
       popupClose = document.querySelector(".popup-close");
+
+    popup.style.opacity = 0;
+
+    const openPopUp = () => {
+      if (hightWindow > 768) {
+        const animate = ({ timing, draw, duration }) => {
+          let start = performance.now();
+
+          requestAnimationFrame(function animate(time) {
+            // timeFraction изменяется от 0 до 1
+            let timeFraction = (time - start) / duration;
+            if (timeFraction > 1) {
+              timeFraction = 1;
+            }
+            // вычисление текущего состояния анимации
+            let progress = timing(timeFraction);
+
+            draw(progress); // отрисовать её
+
+            if (timeFraction < 1) {
+              requestAnimationFrame(animate);
+            }
+          });
+        };
+
+        animate({
+          duration: 1000,
+          timing(timeFraction) {
+            return Math.pow(timeFraction, 2);
+          },
+          draw(progress) {
+            popup.style.opacity = progress * 1;
+          },
+        });
+      }
+    };
 
     popupBtn.forEach((elem) => {
       elem.addEventListener("click", () => {
-        popup.style.display = "block";
+        if (!popup.style.display || popup.style.display === "none") {
+          openPopUp();
+          popup.style.display = "initial";
+        }
       });
     });
     popupClose.addEventListener("click", () => {
