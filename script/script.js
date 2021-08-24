9772d813dd6ecd2d4fa5ce7b4eb2866d794158ba
Yauheny Bychkou <375445433672@yandex.by>
2021-08-24 12:39:36 +0300

    mistake

--- script/script.js
+++ script/script.js
@@ -46,103 +46,29 @@ window.addEventListener("DOMContentLoaded", function () {
 
   const toggleMenu = () => {
     const btnMenu = document.querySelector(".menu"),
-      menu = document.querySelector("menu"),
-      main = document.querySelector("main"),
-      closeBtn = document.querySelector(".close-btn"),
-      menuItems = menu.querySelectorAll("ul>li"),
-      hightWindow = window.innerWidth,
-      total = 46,
-      count = -100;
+      menu = document.querySelector("menu");
 
-    const openModal = () => {
-      if (hightWindow > 768) {
-        const animate = ({ timing, draw, duration }) => {
-          let start = performance.now();
-
-          requestAnimationFrame(function animate(time) {
-            // timeFraction изменяется от 0 до 1
-            let timeFraction = (time - start) / duration;
-            if (timeFraction > 1) {
-              timeFraction = 1;
-            }
-            // вычисление текущего состояния анимации
-            let progress = timing(timeFraction);
-
-            draw(progress); // отрисовать её
-
-            if (timeFraction < 1) {
-              requestAnimationFrame(animate);
-            }
-          });
-        };
-        animate({
-          duration: 1000,
-          timing(timeFraction) {
-            return timeFraction;
-          },
-          draw(progress) {
-            menu.style.transform = `translateX(${progress * (total + 100) - 100}%)`;
-          },
-        });
-      }
+    const handlerMenu = () => {
+      menu.classList.toggle("active-menu");
     };
-    const closeModal = () => {
-      const animate = ({ timing, draw, duration }) => {
-        let start = performance.now();
 
-        requestAnimationFrame(function animate(time) {
-          // timeFraction изменяется от 0 до 1
-          let timeFraction = (time - start) / duration;
-          if (timeFraction > 1) {
-            timeFraction = 1;
-          }
-          // вычисление текущего состояния анимации
-          let progress = timing(timeFraction);
+    btnMenu.addEventListener("click", handlerMenu);
 
-          draw(progress); // отрисовать её
+    menu.addEventListener("click", (event) => {
+      let target = event.target;
+      target = target.closest(".close-btn");
 
-          if (timeFraction < 1) {
-            requestAnimationFrame(animate);
-          }
-        });
-      };
-      animate({
-        duration: 1000,
-        timing(timeFraction) {
-          return timeFraction;
-        },
-        draw(progress) {
-          menu.style.transform = `translateX(${progress * count}%)`;
-        },
-      });
-    };
-    btnMenu.addEventListener("click", function () {
-      if (!menu.style.transform || menu.style.transform === `translateX(-100%)`) {
-        openModal();
-      } else if (menu.style.transform === `translateX(46%)`) {
-        closeModal();
-      }
-    });
-    closeBtn.addEventListener("click", function () {
-      closeModal();
-    });
-    menuItems.forEach((elem) => {
-      elem.addEventListener("click", function () {
-        closeModal();
-      });
-    });
-
-    document.querySelectorAll('a[href^="#"').forEach((link) => {
-      link.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        const blockID = this.getAttribute("href").substr(1);
-
+      if (target) {
+        handlerMenu();
+      } else {
+        handlerMenu();
+        event.preventDefault();
+        const blockID = event.target.getAttribute("href").substr(1);
         document.getElementById(blockID).scrollIntoView({
           behavior: "smooth",
           block: "start",
         });
-      });
+      }
     });
   };
 
@@ -151,8 +77,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const togglePopUp = () => {
     const popup = document.querySelector(".popup"),
       popupBtn = document.querySelectorAll(".popup-btn"),
-      hightWindow = window.innerWidth,
-      popupClose = document.querySelector(".popup-close");
+      hightWindow = window.innerWidth;
 
     popup.style.opacity = 0;
 
@@ -179,7 +104,7 @@ window.addEventListener("DOMContentLoaded", function () {
         };
 
         animate({
-          duration: 1000,
+          duration: 500,
           timing(timeFraction) {
             return Math.pow(timeFraction, 2);
           },
@@ -198,9 +123,50 @@ window.addEventListener("DOMContentLoaded", function () {
         }
       });
     });
-    popupClose.addEventListener("click", () => {
-      popup.style.display = "none";
+
+    popup.addEventListener("click", (event) => {
+      let target = event.target;
+      if (target.classList.contains("popup-close")) {
+        popup.style.display = "none";
+      } else {
+        target = target.closest(".popup-content");
+        if (!target) {
+          popup.style.display = "none";
+        }
+      }
     });
   };
   togglePopUp();
+
+  const tabs = () => {
+    const tabHeader = document.querySelector(".service-header"),
+      tab = tabHeader.querySelectorAll(".service-header-tab"),
+      tabContent = document.querySelectorAll(".service-tab");
+
+    const toggleTabContent = (index) => {
+      for (let i = 0; i < tabContent.length; i++) {
+        if (index === i) {
+          tab[i].classList.add("active");
+          tabContent[i].classList.remove("d-none");
+        } else {
+          tab[i].classList.remove("active");
+          tabContent[i].classList.add("d-none");
+        }
+      }
+    };
+    tabHeader.addEventListener("click", (event) => {
+      let target = event.target;
+
+      target = target.closest(".service-header-tab");
+
+      if (target) {
+        tab.forEach((item, i) => {
+          if (item === target) {
+            toggleTabContent(i);
+          }
+        });
+      }
+    });
+  };
+  tabs();
 });
