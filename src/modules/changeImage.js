3f3e553173ed2823004ddbdae75619802b9ce3d1
Yauheny Bychkou <375445433672@yandex.by>
2021-09-07 08:51:07 +0300

    fix mistake change image

--- src/modules/changeImage.js
+++ src/modules/changeImage.js
@@ -1,8 +1,13 @@
 const changeImage = () => {
   let image = document.querySelectorAll(".command__photo");
   image.forEach((elem) => {
+    let source = elem.src;
+    let dataSource = elem.dataset.img;
     elem.addEventListener("mouseenter", (e) => {
-      e.target.src = e.target.dataset.img;
+      e.target.src = dataSource;
+    });
+    elem.addEventListener("mouseleave", (e) => {
+      e.target.src = source;
     });
   });
 };
