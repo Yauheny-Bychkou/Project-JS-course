b900795242c9109673c28395d6b5640196d630dd
Yauheny Bychkou <375445433672@yandex.by>
2021-09-08 15:16:32 +0300

    add new slider

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import changeImage from "./modules/changeImage";
 import validateInput from "./modules/validateInput";
 import calc from "./modules/calc";
 import sendForm from "./modules/sendForm";
+import SliderCarusel from "./modules/sliderCarusel";
 
 countTimer("09 september 2021");
 
@@ -26,3 +27,27 @@ validateInput();
 calc(100);
 
 sendForm();
+
+const carousel = new SliderCarusel({
+  main: ".companies-wrapper",
+  wrap: ".companies-hor",
+  // prev: "#test-left",
+  // next: "#test-right",
+  slidesToShow: 4,
+  infinity: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      slidesToShow: 3,
+    },
+    {
+      breakpoint: 768,
+      slidesToShow: 2,
+    },
+    {
+      breakpoint: 576,
+      slidesToShow: 1,
+    },
+  ],
+});
+carousel.init();
